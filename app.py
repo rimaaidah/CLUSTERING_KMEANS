b938ad574c7181f75066f18429f91c7e768fd5fe
rimaaidah <rimaaidah028@gmail.com>
2025-05-23 13:11:56 +0700

    Menambahkan Procfile dan memperbarui app.py

--- app.py
+++ app.py
@@ -50,4 +50,5 @@ def download_gambar(jenis):
     return send_file(path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
