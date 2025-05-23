f46132edcb1f8e921422f3f74773252171ed30c5
rimaaidah <rimaaidah028@gmail.com>
2025-05-23 13:40:35 +0700

    memperbarui app.py

--- app.py
+++ app.py
@@ -50,5 +50,4 @@ def download_gambar(jenis):
     return send_file(path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-
+    app.run(debug=True)
